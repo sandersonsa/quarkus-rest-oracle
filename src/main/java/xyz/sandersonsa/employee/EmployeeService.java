ac4c54c5f4cd80346814b7ed52109d537bb92f56
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2025-03-31 21:01:39 -0300

    initial commit github 7

--- src/main/java/xyz/sandersonsa/employee/EmployeeService.java
+++ src/main/java/xyz/sandersonsa/employee/EmployeeService.java
@@ -20,14 +20,21 @@ public class EmployeeService {
         return counter.get();
     }
 
-    @Scheduled(every="1s")     
-    void increment() {
+    @Scheduled(every="60s")     
+    void enviarRegistroTimer() {
         int contador = counter.incrementAndGet(); 
+        for (int i=0; i < 1000; i++) {
+            System.out.println("Enviando registro: " + contador);
+            enviarRegistro(contador);
+        }
+    }
+
+    private void enviarRegistro(Integer id) {  
         Employee c = new Employee();
         c.setEmployeeName("José Roberto");
         c.setCity("Madrid");
-        c.setId(Long.valueOf(contador));
-        employeeRepository.save(c);
+        c.setId(Long.valueOf(id));
+        employeeRepository.save(c); 
     }
 
 }
